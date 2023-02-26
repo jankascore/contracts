43f010ce944106c0dc1b1287a888c63e734f8bab
Dan Simmons <dan@simmons.io>
2023-02-26 10:37:45 -0700

    Started prepping contract/tests for implementation

--- test/JankaProtocol.ts
+++ test/JankaProtocol.ts
@@ -1,121 +1,116 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+// import { parseEther, parseUnits } from "ethers/lib/utils";
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
+describe("JankaProtocol", () => {
+  async function setupFixture() {
+    const [deployer, otherAccount] = await ethers.getSigners();
+
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
 
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const JankaProtocol = await ethers.getContractFactory("JankaProtocol");
+    const janka = await JankaProtocol.deploy(unlockTime, {
+      value: lockedAmount,
+    });
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { janka, unlockTime, lockedAmount, deployer, otherAccount };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+  describe("Deployment", () => {
+    it("Should set the right unlockTime", async () => {
+      const { janka, unlockTime } = await loadFixture(setupFixture);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      expect(await janka.unlockTime()).to.equal(unlockTime);
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    it("Should set the right owner", async () => {
+      const { janka, deployer } = await loadFixture(setupFixture);
 
-      expect(await lock.owner()).to.equal(owner.address);
+      expect(await janka.owner()).to.equal(deployer.address);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+    it("Should receive and store the funds to janka", async () => {
+      const { janka, lockedAmount } = await loadFixture(setupFixture);
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
+      expect(await ethers.provider.getBalance(janka.address)).to.equal(
         lockedAmount
       );
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
+    it("Should fail if the unlockTime is not in the future", async () => {
       // We don't use the fixture here because we want a different deployment
       const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+      const JankaProtocol = await ethers.getContractFactory("JankaProtocol");
+      await expect(
+        JankaProtocol.deploy(latestTime, { value: 1 })
+      ).to.be.revertedWith("Unlock time should be in the future");
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("Withdrawals", () => {
+    describe("Validations", () => {
+      it("Should revert with the right error if called too soon", async () => {
+        const { janka } = await loadFixture(setupFixture);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
+        await expect(janka.withdraw()).to.be.revertedWith(
           "You can't withdraw yet"
         );
       });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
+      it("Should revert with the right error if called from another account", async () => {
+        const { janka, unlockTime, otherAccount } = await loadFixture(
+          setupFixture
         );
 
         // We can increase the time in Hardhat Network
         await time.increaseTo(unlockTime);
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+        // We use janka.connect() to send a transaction from another account
+        await expect(janka.connect(otherAccount).withdraw()).to.be.revertedWith(
           "You aren't the owner"
         );
       });
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async () => {
+        const { janka, unlockTime } = await loadFixture(setupFixture);
 
         // Transactions are sent using the first signer by default
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw()).not.to.be.reverted;
+        await expect(janka.withdraw()).not.to.be.reverted;
       });
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
+    describe("Events", () => {
+      it("Should emit an event on withdrawals", async () => {
+        const { janka, unlockTime, lockedAmount } = await loadFixture(
+          setupFixture
         );
 
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
+        await expect(janka.withdraw())
+          .to.emit(janka, "Withdrawal")
           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
       });
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
+    describe("Transfers", () => {
+      it("Should transfer the funds to the owner", async () => {
+        const { janka, unlockTime, lockedAmount, deployer } = await loadFixture(
+          setupFixture
         );
 
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
+        await expect(janka.withdraw()).to.changeEtherBalances(
+          [deployer, janka],
           [lockedAmount, -lockedAmount]
         );
       });
