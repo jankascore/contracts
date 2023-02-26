cf18a18a5b64c138f1128e18f12c5d5eb7968530
Dan Simmons <dan@simmons.io>
2023-02-26 12:13:14 -0700

    Made an initial pass at attestation behavior.

--- contracts/JankaProtocol.sol
+++ contracts/JankaProtocol.sol
@@ -4,5 +4,49 @@ pragma solidity 0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract JankaProtocol is Ownable {
-    constructor() {}
+    /// A duration of time during which an attested score is able to be challenged.
+    uint256 public constant CHALLENGE_WINDOW = 2 hours;
+
+    /// An amount of at-risk Ether provided with a score attestation to thwart bad actors.
+    uint256 public constant REQUIRED_ATTESTATION_STAKE = 0.01 ether;
+
+    struct Attestation {
+        uint8 score;
+        uint256 finalizationTime;
+    }
+
+    mapping (address => Attestation) public attestations;
+
+    /// Allows an EOA to attest to a score, subject to challenges for `CHALLENGE_WINDOW`.
+    /// @param _score An integer score ranging from 0-100 inclusive.
+    /// @return _finalizationTime The Unix timestamp at which the attestation is considered final (without challenge).
+    function attest(
+        uint8 _score
+    ) external payable returns (uint256 _finalizationTime) {
+        /// Revert straight away if we know the score is bogus.
+        if (_score > 100) revert InvalidScore(0, 100, _score);
+
+        /// Verify that the caller has provided sufficient at-risk Ether.
+        if (msg.value < REQUIRED_ATTESTATION_STAKE)
+            revert InsufficientStake(REQUIRED_ATTESTATION_STAKE, msg.value);
+
+        uint256 finalizationTime = block.timestamp + CHALLENGE_WINDOW;
+        attestations[msg.sender] = Attestation({
+            score: _score,
+            finalizationTime: finalizationTime
+        });
+
+        emit ScoreAttested(msg.sender, _score, finalizationTime);
+        return finalizationTime;
+    }
+
+    event ScoreAttested(
+        address indexed attester,
+        uint8 score,
+        uint256 finalizationTime
+    );
+
+    error InsufficientStake(uint256 amountExpected, uint256 amountProvided);
+    error InvalidScore(uint8 min, uint8 max, uint8 provided);
 }
+
