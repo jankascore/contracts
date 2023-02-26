846c1c14d3aec2db8bfc9f0cd4989c259562d22c
Dan Simmons <dan@simmons.io>
2023-02-25 19:17:09 -0700

    Added additional Hardhat config, using Goerli.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,40 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    hardhat: {
+      accounts: {
+        count: 20,
+        accountsBalance: "10000000000000000000000", // 10ETH (Default)
+      },
+    },
+  },
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
